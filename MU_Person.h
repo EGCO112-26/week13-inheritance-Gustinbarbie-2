fcbb74913d98bcd07502f17c5ac169ca30f02001
Gustinbarbie <kittapatoo@gmail.com>
2026-04-07 07:42:48 +0000

    work

--- MU_Person.h
+++ MU_Person.h
@@ -1,34 +1,31 @@
 #include "NODE.h"
-
+#include <string>
+#include <iostream>
 
 class MU_person:public NODE{
 private: 
   
 protected:
   long id;
-	string name;
+  std::string name;
 
 public:
-	MU_person(long=001 ,string ="Prapaporn");
+  MU_person(long=001 ,std::string ="Prapaporn");
   void display_person();
   ~MU_person();
 };
 
-
-
 MU_person::~MU_person(){
-  cout<<"Destructor id="<<id<<endl;
+  std::cout<<"Destructor id="<<id<<std::endl;
 }
-MU_person::MU_person(long id,string n):NODE(id){
-   	  // Set up all MU_Person values
-        name=n;
-         cout<<"MU person constructor "<<id<<endl;
+MU_person::MU_person(long id, std::string n):NODE(id){
+    name=n;
+    std::cout<<"MU person constructor "<<id<<std::endl;
 }
 
 void MU_person::display_person()
 {
-  cout<<"Info :"<<endl;
-  cout<<"id:"<<id<<endl;
-  cout<<"Name : "<<name<<endl;
-    
+  std::cout<<"Info :"<<std::endl;
+  std::cout<<"id:"<<id<<std::endl;
+  std::cout<<"Name : "<<name<<std::endl;
 }
